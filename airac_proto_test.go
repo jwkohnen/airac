b607fab05147248e3435d22e1b616f6dcd8840a6
Johannes Kohnen <wjkohnen@users.noreply.github.com>
2017-07-09 14:11:27 +0000

    Add a protobuf schema and convert wrappers

---
+++ airac_proto_test.go
@@ -0,0 +1,43 @@
+// +build proto
+
+/*
+ * Copyright (c) 2017 Johannes Kohnen <wjkohnen@users.noreply.github.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package airac
+
+import (
+	"math"
+	"testing"
+)
+
+func TestProto(t *testing.T) {
+	for want := Airac(0); want < FromStringMust("9213"); want++ {
+		p := want.ToProto()
+		got := FromProto(p)
+		if want != got {
+			t.Errorf("Want %v, got %x", want, got)
+		}
+	}
+}
+
+func TestProtoOverflow(t *testing.T) {
+	want := Airac(0)
+	p := AiracMessage{math.MaxUint16 + 1}
+	got := FromProto(p)
+	if got != want {
+		t.Errorf("Want %s, got %s", want, got)
+	}
+}
