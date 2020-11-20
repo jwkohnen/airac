0f11ff6ba9c6864ba49b54278e44c96a0b3bcda7
Johannes Kohnen <jwkohnen-github@ko-sys.com>
2020-11-20 17:15:33 +0100

    Improve test coverage
    no comment :)

--- doc.go
+++ doc.go
@@ -30,33 +30,34 @@ Licensed under the Apache License, Version 2.0.
 package airac
 
 // nolint:godox
-func _() {
-	/* BUG(jwkohnen): The two digit year identifier of the FromString method will
-	   interpret the year as between 1964 and 2063. Other methods than FromString do
-	   not show this range restriction. This time window is more or less arbitrary and
-	   may change. */
+/* BUG(jwkohnen): The two digit year identifier of the FromString method will
+   interpret the year as between 1964 and 2063. Other methods than FromString do
+   not show this range restriction. This time window is more or less arbitrary and
+   may change. */
 
-	/* BUG(jwkohnen): This package assumes that AIRAC cycles are effective from
-	   the effective date at 00:00:00 UTC until 27 days later at 23:59:59.999999999
-	   UTC. That is not correct:
+// nolint:godox
+/* BUG(jwkohnen): This package assumes that AIRAC cycles are effective from
+   the effective date at 00:00:00 UTC until 27 days later at 23:59:59.999999999
+   UTC. That is not correct:
 
-	   ICAO DOC 8126, 6th Edition (2003), paragraph 2.6.4:
-	     "In addition to the use of a predetermined schedule of effective AIRAC dates,
-	     Coordinated Universal Time (UTC) must also be used to indicate the time when
-	     the AIRAC information will become effective. Since Annex 15, paragraph 3.2.3
-	     specifies that the Gregorian calendar and UTC must be used as the temporal
-	     reference system for international civil aviation, in addition to AIRAC
-	     dates, 00:01 UTC must be used to indicate the time when the AIRAC-based
-	     information will become effective."
+   ICAO DOC 8126, 6th Edition (2003), paragraph 2.6.4:
+     "In addition to the use of a predetermined schedule of effective AIRAC dates,
+     Coordinated Universal Time (UTC) must also be used to indicate the time when
+     the AIRAC information will become effective. Since Annex 15, paragraph 3.2.3
+     specifies that the Gregorian calendar and UTC must be used as the temporal
+     reference system for international civil aviation, in addition to AIRAC
+     dates, 00:01 UTC must be used to indicate the time when the AIRAC-based
+     information will become effective."
 
-	   However I won't "fix" this, because that may just confuse users. */
+   However I won't "fix" this, because that may just confuse users. */
 
-	/* BUG(jwkohnen): Calculations that include calendar dates before the internal
-	   epoch (1901-01-10; 63 years before the AIRAC system was introduced by the ICAO)
-	   and after year 2192 may silently produce wrong data. */
+// nolint:godox
+/* BUG(jwkohnen): Calculations that include calendar dates before the internal
+   epoch (1901-01-10; 63 years before the AIRAC system was introduced by the ICAO)
+   and after year 2192 may silently produce wrong data. */
 
-	/* BUG(jwkohnen): This package only provides calculations on effective dates,
-	   not publication or reception dates etc. Although effective dates are clearly
-	   defined and are consistent at least between 1998 until 2020, the derivative
-	   dates changed historically.[citation needed] */
-}
+// nolint:godox
+/* BUG(jwkohnen): This package only provides calculations on effective dates,
+   not publication or reception dates etc. Although effective dates are clearly
+   defined and are consistent at least between 1998 until 2020, the derivative
+   dates changed historically.[citation needed] */
